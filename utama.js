a2f7cf0241c6c5aa59fa3e0d6e994105e7241f00
ngoclog <62193504+ngoclog@users.noreply.github.com>
2020-03-15 13:43:16 +0700

    Create utama.js

---
+++ utama.js
@@ -0,0 +1,7 @@
+window.cookieconsent_options = {
+	"message" : "<h1>Peringatan Konten</h1><br><br>Blog yang akan Anda lihat mungkin memuat konten yang hanya sesuai untuk orang dewasa. Secara umum, Google tidak mengkaji dan juga tidak mendukung konten blog ini atau manapun. Untuk informasi selengkapnya tentang kebijakan konten Ahli Ngocok, kunjungi ",
+	"dismiss" : "Saya Mengerti dan Ingin Melanjutkan",
+	"learnMore" : "Ketentuan Layanan",
+	"link" : "https://www.blogger.com/terms.g",
+	"theme" : false
+};
