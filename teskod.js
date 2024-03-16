c4203728d5da3cf049f4a058139f0c8d7fce38c9
ngoclog <62193504+ngoclog@users.noreply.github.com>
2024-03-16 19:32:17 +0700

    Update teskod.js

--- teskod.js
+++ teskod.js
@@ -1,30 +1,24 @@
 // Dekode teks
-function decodeText(encodedInput) {
-    var encodedData = {
-        "ABCD1234": "https://www.example.com",
-        "EFGH5678": "https://www.anotherexample.com"
-    };
-
-    if (encodedData[encodedInput]) {
-        return encodedData[encodedInput];
-    } else {
-        return null;
-    }
-}
-
-// Fungsi untuk memanggil fungsi decodeText
-function generateLink() {
+function decodeText() {
     try {
         var encodedInput = document.getElementById("encodedInput").value;
         var decodedOutput = document.getElementById("decodedOutput");
+        var thankYouMessage = document.getElementById("thankYouMessage");
 
-        var decodedLink = decodeText(encodedInput);
-        if (decodedLink) {
-            decodedOutput.href = decodedLink;
-            decodedOutput.textContent = decodedLink;
+        var encodedData = {
+            "BKR-123": "https://www.example.com",
+            // Tambahkan pasangan kunci-nilai lain di sini sesuai kebutuhan Anda
+        };
+
+        if (encodedData[encodedInput]) {
+            var originalText = encodedData[encodedInput];
+            decodedOutput.href = originalText;
+            decodedOutput.textContent = originalText;
+            thankYouMessage.style.display = "block"; // Menampilkan pesan terima kasih
         } else {
             decodedOutput.href = "#";
             decodedOutput.textContent = "Teks tidak dapat didekode.";
+            thankYouMessage.style.display = "none"; // Sembunyikan pesan terima kasih jika tidak ada hasil dekode
         }
     } catch (error) {
         console.error("Terjadi kesalahan:", error);
